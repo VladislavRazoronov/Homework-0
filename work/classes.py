b9af84d83d6d04a6b90e62dfcac80e7dccfc3282
Vladislav Razoronov <razoronov@ucu.edu.ua>
2020-05-11 15:59:55 +0300

    update

--- work/classes.py
+++ work/classes.py
@@ -1,54 +1,6 @@
-class Food:
-    """
-    Class for representing food with calories
-    and nutrients
-    """
-    def __init__(self, name ,cal, carb, prot, fat):
-        self.name = name
-        self.calories = cal
-        self.carbohydrates = carb
-        self.proteins = prot
-        self.fat = fat
+from abstract_class import AbstractFoodClass
 
-    def set_name(self, name):
-        """
-        Set food name
-        """
-        self.name = name
-
-    def set_calories(self, calories):
-        """
-        Set food calories to given number
-        """
-        self.calories = calories
-    
-    def set_carbohydrates(self, carbohydrates):
-        """
-        Set food carbohydrates to given number
-        """
-        self.carbohydrates = carbohydrates
-    
-    def set_proteins(self, proteins):
-        """
-        Set food proteins to given number
-        """
-        self.proteins = proteins
-    
-    def set_fat(self, fat):
-        """
-        Set food fat to given number
-        """
-        self.fat = fat
-    
-    def __eq__(self, other):
-        return self.name == other.name and self.calories == other.calories
-    
-    def __str__(self):
-        return f'{self.name} has {self.calories} calories, {self.carbohydrates}' +\
-        f'g. carbohydrates, {self.fat}g. of fat and {self.proteins}g. of proteins'
-
-
-class Recipe(Food):
+class Recipe(AbstractFoodClass):
     """
     Class for representing recipes with calories, 
     nutrients, ingredients and link to its source
@@ -67,7 +19,7 @@ class Recipe(Food):
     def find_ingredient(self, ingredient):
         return self.ingredients[ingredient]
 
-class Ingredient(Food):
+class Ingredient(AbstractFoodClass):
     """
     Class for representing ingredient with calories,
     nutrients and list of recipes it is used in
