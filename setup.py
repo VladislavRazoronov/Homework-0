f9319a75bdd22979c232a23a11abc3a6ffb0351b
Vladislav Razoronov <razoronov@ucu.edu.ua>
2020-05-19 18:42:21 +0300

    added_distributive

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import pydoc
+setup(
+    name='Ingredient optimiser',
+    version='1.0',
+    package_dir={'':'work'},
+    packages=[''],
+    author='Vladislav razoronov',
+    author_email='razoronov@ucu.edu.ua',
+    url='https://github.com/VladislavRazoronov/Ingredient-optimiser',
+    description='A program for analyzing recipes'
+    )
